43e4bbaae0f6f64de027c60f12f7a1cd97d9b8b1
Sateesh Raghuwanshi <aasra.sateesh@gmail.com>
2020-01-29 08:13:21 +0530

    Finalize chat message send and replies

--- src/app/chatbox/chatbox.component.ts
+++ src/app/chatbox/chatbox.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment'; // 
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-chatbox',
@@ -7,26 +8,54 @@ import * as moment from 'moment'; //
   styleUrls: ['./chatbox.component.css']
 })
 export class ChatboxComponent implements OnInit {
-  model: any = {}; chatMessages = {}; currentTime = '';
+  model: any = {}; chatMessages = {}; threadID = ''; chatThreadMessages = [];
 
   constructor() { }
 
   ngOnInit() {
-    this.currentTime = moment().format('LT');
+    // this.currentTime = moment().format('LT');
   }
 
   sendMessage() {
-    console.log(this.model.message);
-    this.chatMessages[ new Date().getTime() ] = { // https://stackoverflow.com/questions/31490713/iterate-over-object-in-angular
-      'message' : this.model.message,
-      'dateTime' : new Date()
-    };
+    if ( this.model.hasOwnProperty('message') && this.model.message.length > 0 ) {
+      this.chatMessages[ new Date().getTime() ] = { // https://stackoverflow.com/questions/31490713/iterate-over-object-in-angular
+        'message' : this.model.message,
+        'dateTime' : new Date()
+      };
+    }
 
     this.model.message = '';
   }
 
+  postReply(){
+    if ( this.model.hasOwnProperty('replyMessage') && this.model.replyMessage.length > 0 ) {
+      let replymsg = {
+        'message' : this.model.replyMessage,
+        'dateTime' : new Date()
+      };
+
+      let replies = this.chatMessages[this.threadID]['replies'] || [];
+      replies.push(replymsg);
+
+      this.chatMessages[this.threadID]['replies'] = replies;
+
+      this.chatThreadMessages = this.chatMessages[this.threadID]['replies'];
+
+      this.model.replyMessage = '';
+    }
+  }
+
+  openChatReply(threadID){
+    this.threadID = threadID;
+    this.chatThreadMessages = this.chatMessages[this.threadID]['replies'];
+  }
+
+  closeChatReply(){
+    this.threadID = '';
+  }
+
   showDate(dateTime, type){
-    if('mainThread' == type) {
+    if( 'mainThread' == type ) {
       return moment(dateTime).format('LT');
     }
 
